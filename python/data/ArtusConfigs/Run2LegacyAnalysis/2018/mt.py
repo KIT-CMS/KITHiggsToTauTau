10d85b9a73f8b42b53a35ea175fe6da050c8b127
Olena Hlushchenko <greyxray@gmail.com>
2019-08-05 17:18:16 +0200

    typo in collections

--- python/data/ArtusConfigs/Run2LegacyAnalysis/2018/mt.py
+++ python/data/ArtusConfigs/Run2LegacyAnalysis/2018/mt.py
@@ -389,10 +389,6 @@ def build_config(nickname, **kwargs):
   # Subanalyses settings
   if btag_eff:
     config["Processors"] = copy.deepcopy(config["ProcessorsBtagEff"])
-
-
-  if btag_eff:
-    config["Processors"] = copy.deepcopy(config["ProcessorsBtagEff"])
     if pipelines != ['nominal']:
         raise Exception("There is no use case for calculating btagging efficiency with systematics shifts: %s" % ' '.join(pipelines))
 
