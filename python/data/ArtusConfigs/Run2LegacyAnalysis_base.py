a5cd1d034d0ddbdcf6c4b1f6c7141fea594bab7d
swozniewski <sebastian.wozniewski@kit.edu>
2019-11-13 16:54:30 +0100

    add preliminary lepton to tau fake rate SFs for deepTau

--- python/data/ArtusConfigs/Run2LegacyAnalysis_base.py
+++ python/data/ArtusConfigs/Run2LegacyAnalysis_base.py
@@ -173,20 +173,20 @@ def build_config(nickname, **kwargs):
       exit(1)
 
   if year == 2016:
-    config["SimpleMuTauFakeRateWeightLoose"] = [1.22, 1.12, 1.26, 1.22, 2.39]
-    config["SimpleMuTauFakeRateWeightTight"] = [1.47, 1.55, 1.33, 1.72, 2.50]
-    config["SimpleEleTauFakeRateWeightVLoose"] = [1.21, 1.38]
-    config["SimpleEleTauFakeRateWeightTight"] = [1.40, 1.90]
+    config["SimpleMuTauFakeRateWeightLoose"] = [1.25, 0.96, 1.29, 0.92, 5.00]
+    config["SimpleMuTauFakeRateWeightTight"] = [1.38, 0.72, 1.34, 1.03, 5.00]
+    config["SimpleEleTauFakeRateWeightVLoose"] = [1.01, 0.994]
+    config["SimpleEleTauFakeRateWeightTight"] = [0.60, 0.88]
   elif year == 2017:
-    config["SimpleMuTauFakeRateWeightLoose"] = [1.06, 1.02, 1.10, 1.03, 1.94]
-    config["SimpleMuTauFakeRateWeightTight"] = [1.17, 1.29, 1.14, 0.93, 1.61]
-    config["SimpleEleTauFakeRateWeightVLoose"] = [1.09, 1.19]
-    config["SimpleEleTauFakeRateWeightTight"] = [1.80, 1.53]
+    config["SimpleMuTauFakeRateWeightLoose"] = [1.12, 0.76, 0.99, 0.75, 4.44]
+    config["SimpleMuTauFakeRateWeightTight"] = [0.92, 0.79, 0.67, 1.07, 4.08]
+    config["SimpleEleTauFakeRateWeightVLoose"] = [1.00, 1.03]
+    config["SimpleEleTauFakeRateWeightTight"] = [1.38, 1.83]
   elif year == 2018:
     config["SimpleMuTauFakeRateWeightLoose"] = [1.05, 0.96, 1.06, 1.45, 1.75]
-    config["SimpleMuTauFakeRateWeightTight"] = [1.23, 1.37, 1.12, 1.84, 2.01]
-    config["SimpleEleTauFakeRateWeightVLoose"] = [1.089, 1.189]
-    config["SimpleEleTauFakeRateWeightTight"] = [1.78, 1.55]
+    config["SimpleMuTauFakeRateWeightTight"] = [1.20, 0.82, 0.91, 0.80, 3.62]
+    config["SimpleEleTauFakeRateWeightVLoose"] = [0.986, 1.020]
+    config["SimpleEleTauFakeRateWeightTight"] = [1.38, 1.03]
 
   if re.search("GluGluHToTauTauM125", nickname):
     config["ggHNNLOweightsRootfile"] = "$CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/root/NNLOWeights/NNLOPS_reweight.root" # same for all years?
