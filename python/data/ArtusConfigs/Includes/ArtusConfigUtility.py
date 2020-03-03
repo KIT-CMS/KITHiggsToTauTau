1a97e3e9343fa6b97a30e5a90be008b448ed768d
Olena Hlushchenko <greyxray@gmail.com>
2020-03-03 17:38:48 +0100

    use update for the pipelines to avoid appending of lists where they should be rewritten

--- python/data/ArtusConfigs/Includes/ArtusConfigUtility.py
+++ python/data/ArtusConfigs/Includes/ArtusConfigUtility.py
@@ -6,6 +6,7 @@ import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
 import re
+import copy
 import json
 import Artus.Utility.jsonTools as jsonTools
 import Kappa.Skimming.datasetsHelperTwopz as datasetsHelperTwopz
@@ -14,6 +15,7 @@ def apply_uncertainty_shift_configs(configname, config, systs):
   config_with_systs = jsonTools.JsonDict()
   for key, syst in systs.items():
     longkey = configname + "_" + key
-    config_with_systs[longkey] = jsonTools.JsonDict(syst)
-    config_with_systs[longkey] += config
+    config_with_systs[longkey] = copy.deepcopy(config)
+    config_with_systs[longkey].update(jsonTools.JsonDict(syst))
+
   return config_with_systs
