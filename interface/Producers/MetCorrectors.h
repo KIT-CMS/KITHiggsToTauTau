a8509dc787d3e6f9d49f7b785a165c0158c8031e
Janek Bechtel <janek.bechtel@kit.edu>
2020-05-14 17:19:39 +0200

    Add NMSSM samples to bosonic MET corrections

--- interface/Producers/MetCorrectors.h
+++ interface/Producers/MetCorrectors.h
@@ -88,7 +88,7 @@ public:
 		}
 
 		// determine process type, trigger several decisions later
-		if (boost::regex_search(settings.GetNickname(), boost::regex("DY.?JetsToLL|(W.?Jets|WG|WGstar)ToLNu|HToTauTau|EWK|HToWW|ttHJetToTT", boost::regex::extended)))
+		if (boost::regex_search(settings.GetNickname(), boost::regex("DY.?JetsToLL|(W.?Jets|WG|WGstar)ToLNu|HToTauTau|EWK|HToWW|ttHJetToTT|NMSSM", boost::regex::extended)))
 		{
 			m_processType = MEtSys::ProcessType::BOSON;
 		}
